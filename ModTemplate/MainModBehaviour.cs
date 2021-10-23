6f3a4a03a572cb34ac1e8a866f9da19bf05fadf9
Vesper <0ooechooo0oooo@gmail.com>
2021-10-23 22:00:45 +0100

    Initial Commit

---
+++ ModTemplate/MainModBehaviour.cs
@@ -0,0 +1,99 @@
+﻿using OWML.ModHelper;
+using OWML.Common;
+using System;
+using UnityEngine;
+using OWML.ModHelper.Events;
+
+namespace OuterWildsPixelShader
+{
+    public class MainModBehaviour : ModBehaviour
+    {
+        RenderTexture cameraTexture;
+        RenderTexture pixelTexture;
+        ComputeShader computeShader;
+        Camera PixelCamera;
+        public float ScaleFactor;
+        public bool NoBlend;
+        Camera NormalCamera { get => Camera.main; }
+        public static MainModBehaviour Instance { get; set; }
+        private void Start()
+        {
+            Instance = this;
+      
+            ModHelper.Console.WriteLine("Skipping splash screen...");
+            var titleScreenAnimation = FindObjectOfType<TitleScreenAnimation>();
+            titleScreenAnimation.SetValue("_fadeDuration", 0);
+            titleScreenAnimation.SetValue("_gamepadSplash", false);
+            titleScreenAnimation.SetValue("_introPan", false);
+            titleScreenAnimation.Invoke("FadeInTitleLogo");
+            ModHelper.Console.WriteLine("Done!");
+
+            ModHelper.Events.Scenes.OnCompleteSceneChange += OnCompleteSceneChange;
+
+            Textures();
+            ComputeShader();
+            CameraWork();
+        }
+
+        private void OnCompleteSceneChange(OWScene oldScene, OWScene newScene)
+        {
+            CameraWork();
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            NoBlend = !config.GetSettingsValue<bool>("Blend");
+            ScaleFactor = config.GetSettingsValue<float>("Pixel scale");         
+        }
+
+        private void Textures()
+        {
+            ModHelper.Console.WriteLine("Creating textures...");
+            pixelTexture = new RenderTexture(1920, 1080, 0);
+            pixelTexture.enableRandomWrite = true;
+            pixelTexture.filterMode = FilterMode.Point;
+            pixelTexture.Create();
+
+            cameraTexture = new RenderTexture(1920, 1080, 0);
+            cameraTexture.enableRandomWrite = true;
+            cameraTexture.filterMode = FilterMode.Point;
+            cameraTexture.Create();
+            ModHelper.Console.WriteLine("Done!");
+        }
+
+        private void ComputeShader()
+        {
+            ModHelper.Console.WriteLine("Loading compute shader...");
+
+            var shaderbundle = ModHelper.Assets.LoadBundle("pixelshader");
+
+            computeShader = shaderbundle.LoadAsset<ComputeShader>("PixelShader");
+            ModHelper.Console.WriteLine("Done!");
+        }
+
+        private void CameraWork()
+        {
+            if (NormalCamera == null) { return; }
+
+            GameObject textureCameraGO = new GameObject();
+            PixelCamera = textureCameraGO.AddComponent<Camera>();
+            textureCameraGO.AddComponent<OWCamera>();
+
+            textureCameraGO.transform.SetParent(NormalCamera.transform);
+            textureCameraGO.transform.position = Vector3.zero;
+            textureCameraGO.transform.rotation = Quaternion.identity;
+            PixelCamera.CopyFrom(NormalCamera);
+            PixelCamera.cullingMask = 1 << 5;
+            NormalCamera.targetTexture = cameraTexture;
+            ModHelper.Console.WriteLine("Done!");
+
+            AddACIIRenderingToCamera(textureCameraGO);
+
+        }
+        private void AddACIIRenderingToCamera(GameObject camera)
+        {
+            camera.AddComponent<ShaderApplier>().Ready(cameraTexture, pixelTexture, computeShader);
+        }
+    }
+}
+
---
+++ ModTemplate/ShaderApplier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OuterWildsPixelShader
+{
+    class ShaderApplier : MonoBehaviour
+    {
+        RenderTexture cameraTexture;
+        RenderTexture pixelTexture;
+        ComputeShader computeShader;
+        bool running;
+       
+        public void Ready(RenderTexture cameraTexture, RenderTexture pixelTexture, ComputeShader computeShader)
+        {
+            this.cameraTexture = cameraTexture;
+            this.pixelTexture = pixelTexture;
+            this.computeShader = computeShader;
+            running = true;
+        }
+        private void Update()
+        {
+            if (!running || Keyboard.current["h"].IsPressed()) { return; }
+            computeShader.SetTexture(0, "cameraTexture", cameraTexture);
+            computeShader.SetTexture(0, "pixelTexture", pixelTexture);
+            computeShader.SetFloat("scaleFactor", MainModBehaviour.Instance.ScaleFactor);
+            computeShader.SetBool("noBlend", MainModBehaviour.Instance.NoBlend);
+            computeShader.Dispatch(0, 2000 / (int)Math.Min(MainModBehaviour.Instance.ScaleFactor * MainModBehaviour.Instance.ScaleFactor, 8 * 8), 
+                1200 / (int)Math.Min(MainModBehaviour.Instance.ScaleFactor * MainModBehaviour.Instance.ScaleFactor, 8 * 8), 1);
+        }
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (!running)
+            {
+                return;
+            }
+            if (Keyboard.current["h"].IsPressed())
+            {
+                Graphics.Blit(cameraTexture, destination);
+            }
+            else
+            {
+                Graphics.Blit(pixelTexture, destination);
+            }
+
+        }
+    }
+}
